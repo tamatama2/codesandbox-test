f9a3b3a61044544b76b7625b867640ee71dcab89
tamatama2 <128953538+tamatama2@users.noreply.github.com>
2023-03-26 17:10:48 +0900

    分割代入まで

--- src/index.js
+++ src/index.js
@@ -1,89 +1,109 @@
-/**
- *
- */
-// var val1 = "var変数";
-// console.log(val1)
+// // /**
+// //  *
+// //  */
+// // // var val1 = "var変数";
+// // // console.log(val1)
 
-// //var変数は上書き可能
-// val="var変数は上書き";
-// console.log(val1);
+// // // //var変数は上書き可能
+// // // val="var変数は上書き";
+// // // console.log(val1);
 
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+// // // var val1 = "var変数を再宣言";
+// // // console.log(val1);
 
-// let val2 = "let変数";
-// console.log(val2)
+// // // let val2 = "let変数";
+// // // console.log(val2)
 
-// // letは上書きが可能
-// val2 = "let変数上書き";
-// console.log(val2);
+// // // // letは上書きが可能
+// // // val2 = "let変数上書き";
+// // // console.log(val2);
 
-// //　letは再宣言不可能
-// let val2 = "let変数を再宣言";
+// // // //　letは再宣言不可能
+// // // let val2 = "let変数を再宣言";
 
-// const val3 = "const変数";
-// console.log(val3);
+// // // const val3 = "const変数";
+// // // console.log(val3);
 
-// //const変数は上書き不可能
-// //va3 = "const変数を上書き"
+// // // //const変数は上書き不可能
+// // // //va3 = "const変数を上書き"
 
-// const val3 = "const変数を再宣言";
+// // // const val3 = "const変数を再宣言";
 
-//constで定義したオブジェクトはプロパティの変更が可能
-// const val4 = {
-//   name: "じゃけぇ",
-//   age: 28,
-// };
-// val4.name = "jak";
-// val4.addres = "hiroshima";
-// console.log(val4);
+// // //constで定義したオブジェクトはプロパティの変更が可能
+// // // const val4 = {
+// // //   name: "じゃけぇ",
+// // //   age: 28,
+// // // };
+// // // val4.name = "jak";
+// // // val4.addres = "hiroshima";
+// // // console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// // const val5 = ["dog", "cat"];
+// // val5[0] = "bird";
+// // val5.push("monkey");
+// // console.log(val5);
 
-/**
- * テンプレート文字列
- * /
-const name = "じゃけぇ";
-const age = 28;
-// 「私の名前はじゃけぇです。年齢は28歳です。」
+// // /**
+// //  * テンプレート文字列
+// //  * /
+// // const name = "じゃけぇ";
+// // const age = 28;
+// // // 「私の名前はじゃけぇです。年齢は28歳です。」
 
-// 従来の方法
-const message1 = "私の名前は"　+ name + "です。年齢は" + "です。"
-$
+// // // 従来の方法
+// // const message1 = "私の名前は"　+ name + "です。年齢は" + "です。"
+// // $
 
-/**
- * 
- */
-//従来の関数
-// function func1(str) {
-//   return str;
-// }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// // /**
+// //  *
+// //  */
+// // //従来の関数
+// // // function func1(str) {
+// // //   return str;
+// // // }
+// // // const func1 = function (str) {
+// // //   return str;
+// // // };
+// // // console.log(func1("func1です"));
 
-//アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// // // //アロー関数
+// // // const func2 = (str) => {
+// // //   return str;
+// // // };
+// // // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
+// // // const func3 = (num1, num2) => num1 + num2;
 
-console.log(func3(10, 20));
+// // // console.log(func3(10, 20));
 
-/**
- * 分割代入
- */
+// // // /**
+// // //  * 分割代入
+// // //  */
 
-const myprofile = {
-  name: "たま",
-  age: 28
-};
+// // // const myprofile = {
+// // //   name: "たま",
+// // //   age: 28
+// // // };
 
-const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}です`;
-console.log(message1);
+// // // const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}です`;
+// // // console.log(message1);
+
+// // // const { name, age } = myprofile;
+// // // const message2 = `名前は${name}です。年齢は${age}です`;
+// // // console.log(message2);
+
+// // const myprofile = ['たま', 28];
+
+// // const message3 = `名前は${myprofile[0]}です。年齢は${myprofile[1]}です`;
+// // console.log(message3);
+
+// // const [name, age] = myprofile;
+// // const message4 = `名前は${name}です。年齢は${age}です`;
+// // console.log(message4)
+
+// /**
+//  *デフォルト値　引数など
+//  */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん!`);
+// sayHello("たま");
