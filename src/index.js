51c56f585deaff10e1dc2d1d0abe0898e8a82f67
tamatama2 <128953538+tamatama2@users.noreply.github.com>
2023-03-26 16:45:47 +0900

    arrow関数まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,89 @@
-import "./styles.css";
+/**
+ *
+ */
+// var val1 = "var変数";
+// console.log(val1)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //var変数は上書き可能
+// val="var変数は上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2)
+
+// // letは上書きが可能
+// val2 = "let変数上書き";
+// console.log(val2);
+
+// //　letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書き不可能
+// //va3 = "const変数を上書き"
+
+// const val3 = "const変数を再宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+// val4.name = "jak";
+// val4.addres = "hiroshima";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
+
+/**
+ * テンプレート文字列
+ * /
+const name = "じゃけぇ";
+const age = 28;
+// 「私の名前はじゃけぇです。年齢は28歳です。」
+
+// 従来の方法
+const message1 = "私の名前は"　+ name + "です。年齢は" + "です。"
+$
+
+/**
+ * 
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+const myprofile = {
+  name: "たま",
+  age: 28
+};
+
+const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}です`;
+console.log(message1);
